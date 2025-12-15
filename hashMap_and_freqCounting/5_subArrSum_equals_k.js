07bca3d805e689652ae70b3c5cbe76c132ba3b8b
shantanu.ss <shantanu.s@kinaracapital.com>
2025-12-15 15:38:49 +0530

    refactor: optimal solution

--- hashMap_and_freqCounting/5_subArrSum_equals_k.js
+++ hashMap_and_freqCounting/5_subArrSum_equals_k.js
@@ -1,15 +1,37 @@
+// Brute Force Approach:
 function subArraySumEqualsK(arr, k) {
-    let count = 0;
-    for (let i=0; i < arr.length; i++) {
-        let sum = 0
-        for (let j=i; j < arr.length; j++) {
-            sum += arr[j]
-            if (sum === k) count ++
-        }
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let sum = 0;
+    for (let j = i; j < arr.length; j++) {
+      sum += arr[j];
+      if (sum === k) count++;
     }
+  }
 
+  return count;
+}
+
+// Optimized Approach using HashMap:
+function subArraySumEqualsKOptimized(nums, k) {
+    const map = new Map()
+    let sum = 0
+    let count = 0
+    //init for sum = 0
+    map.set(0, 1)
+
+    for (let num of nums) {
+        sum += num
+        const diff = sum -k
+
+        if (map.has(diff)) count += map.get(diff)
+        //setting sum and count
+        if (map.has(sum)) map.set(sum, map.get(sum) + 1)
+        else map.set(sum, 0)
+    }
+    
     return count
 }
 
-console.log(subArraySumEqualsK([1, 2, 3], 3)); // 2
+console.log(subArraySumEqualsK([1, 2, 3], 5)); // 2
 console.log(subArraySumEqualsK([1, 1, 1], 2)); // 2
